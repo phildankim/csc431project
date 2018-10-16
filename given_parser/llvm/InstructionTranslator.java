962291ccfa02a651a3159905ab444da0008c2732
Daniel <phildankim@gmail.com>
2018-10-16 13:19:05 -0700

    translator

---
+++ given_parser/llvm/InstructionTranslator.java
@@ -0,0 +1,41 @@
+package llvm;
+
+import java.util.*;
+
+import ast.*;
+import cfg.*;
+
+public class InstructionTranslator {
+
+	public static void translate(Block b, Statement s) {
+
+		if (s instanceof AssignmentStatement) {
+			
+		}
+
+
+
+		// if (e instanceof BinaryExpression) {
+		// 	String opr = e.getOperator().name();
+
+		// 	switch (opr) {
+		// 		case BinaryExpression.Operator.TIMES:
+		// 			return InstructionIcmp
+		// 		case BinaryExpression.Operator.DIVIDE:
+		// 		case BinaryExpression.Operator.PLUS:
+		// 		case BinaryExpression.Operator.MINUS:
+		// 		case BinaryExpression.Operator.LT:
+		// 		case BinaryExpression.Operator.LE
+		// 		case BinaryExpression.Operator.GT:
+		// 		case BinaryExpression.Operator.GE:
+		// 		case BinaryExpression.Operator.EQ:
+		// 		case BinaryExpression.Operator.NE:
+		// 		case BinaryExpression.Operator.AND:
+		// 		case BinaryExpression.Operator.OR:
+		// 		default:
+		// 			throw new IllegalArgumentException();
+		// 	}
+		// }
+	}
+
+}
