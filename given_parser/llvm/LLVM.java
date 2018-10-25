196c339f2e13262fe45c0935cd7ee3956d200031
michaeljohnh <michaelhilomen@gmail.com>
2018-10-25 16:21:09 -0700

    header and footer

--- given_parser/llvm/LLVM.java
+++ given_parser/llvm/LLVM.java
@@ -28,12 +28,26 @@ public class LLVM {
 	}
 
 	public void printInstructions(BufferedWriter writer) throws IOException {
+
+		// header:
+		writer.write("target triple=\"i686\"\n");
+
 		for (Instruction i : globalDecls) {
 			writer.write(i.toString() + "\n");
 		}
 		for (CFG c : this.cfgs) {
 			c.printInstructions(writer);
 		}
+
+		// footer:
+		writer.write("declare i8* @malloc(i32)\n");
+		writer.write("declare void @free(i8*)\n");
+		writer.write("declare i32 @printf(i8*, ...)\n");
+		writer.write("declare i32 @scanf(i8*, ...)\n");
+		writer.write("@.println = private unnamed_addr constant [5 x i8] c\"%ld\\0A\\00\", align 1\n");
+		writer.write("@.print = private unnamed_addr constant [5 x i8] c\"%ld \\00\", align 1\n");
+		writer.write("@.read = private unnamed_addr constant [4 x i8] c\"%ld\\00\", align 1\n");
+		writer.write("@.read_scratch = common global i32 0, align 8\n");
 	}
 
 	public void setDeclInstructions() {
