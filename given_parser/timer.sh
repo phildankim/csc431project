1ce7805b9d73eebd35022d3ad5c876d2cc959f27
Daniel <phildankim@gmail.com>
2018-12-12 12:43:02 -0800

    timer

--- given_parser/timer.sh
+++ given_parser/timer.sh
@@ -5,29 +5,30 @@ make clean;make
 for dir in benchmarks/*; do
    f=${dir##*/}
    echo $f
-   java MiniCompiler $dir/${f}.mini -stack
+   java MiniCompiler $dir/${f}.mini -stack -simp
    clang $dir/${f}.ll -m32
    start=`date +%s.%N`
-   ./a.out < $dir/input > ${f}.txt
+   ./a.out < $dir/input.longer > ${f}.txt
    end=`date +%s.%N`
+   diff ${f}.txt $dir/output
    elapsed="$(bc <<<"$end-$start")"
    echo $elapsed
 
-   java MiniCompiler $dir/${f}.mini -llvm
-   clang $dir/${f}.ll -m32
-   start=`date +%s.%N` 
-   ./a.out < $dir/input > ${f}.txt
-   end=`date +%s.%N`
-   elapsed="$(bc <<<"$end-$start")"
-   echo $elapsed
+   #java MiniCompiler $dir/${f}.mini -llvm
+   #clang $dir/${f}.ll -m32
+   #start=`date +%s.%N` 
+   #./a.out < $dir/input > ${f}.txt
+   #end=`date +%s.%N`
+   #elapsed="$(bc <<<"$end-$start")"
+   #echo $elapsed
 
-   java MiniCompiler $dir/${f}.mini -llvm -sscp -uce
-   clang $dir/${f}.ll -m32
-   start=`date +%s.%N`
-   ./a.out < $dir/input > ${f}.txt
-   end=`date +%s.%N`
-   elapsed="$(bc <<<"$end-$start")"
-   echo $elapsed
+   #java MiniCompiler $dir/${f}.mini -llvm -sscp -uce
+   #clang $dir/${f}.ll -m32
+   #start=`date +%s.%N`
+   #./a.out < $dir/input > ${f}.txt
+   #end=`date +%s.%N`
+   #elapsed="$(bc <<<"$end-$start")"
+   #echo $elapsed
 
    clang -S -O0 -Wno-everything $dir/${f}.c
    start=`date +%s.%N`
