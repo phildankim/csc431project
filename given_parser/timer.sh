a8dcbfa5edc7fe1402cf26037a72e46b078fc193
Daniel Kim <dkim96@unix3.csc.calpoly.edu>
2018-12-12 15:10:34 -0800

    timer

--- given_parser/timer.sh
+++ given_parser/timer.sh
@@ -5,44 +5,55 @@ make clean;make
 for dir in benchmarks/*; do
    f=${dir##*/}
    echo $f
-   java MiniCompiler $dir/${f}.mini -stack -simp
+   #java MiniCompiler $dir/${f}.mini -stack
+   #clang $dir/${f}.ll -m32
+   #start=`date +%s.%N`
+   #./a.out < $dir/input.longer > ${f}.txt
+   #end=`date +%s.%N`
+   #diff ${f}.txt $dir/output.longer
+   #elapsed="$(bc <<<"$end-$start")"
+   #echo 'stack w/o opt: '$elapsed
+
+   #java MiniCompiler $dir/${f}.mini -stack -simp
+   #clang $dir/${f}.ll -m32
+   #start=`date +%s.%N`
+   #./a.out < $dir/input.longer > ${f}.txt
+   #end=`date +%s.%N`
+   #diff ${f}.txt $dir/output.longer
+   #elapsed="$(bc <<<"$end-$start")"
+   #echo 'stack w/ opt: '$elapsed
+
+   java MiniCompiler $dir/${f}.mini -llvm
+   clang $dir/${f}.ll -m32
+   start=`date +%s.%N` 
+   ./a.out < $dir/input.longer > ${f}.txt
+   end=`date +%s.%N`
+   diff ${f}.txt $dir/output.longer
+   elapsed="$(bc <<<"$end-$start")"
+   echo 'llvm w/o opt: '$elapsed
+
+   java MiniCompiler $dir/${f}.mini -llvm -sscp -uce
    clang $dir/${f}.ll -m32
    start=`date +%s.%N`
    ./a.out < $dir/input.longer > ${f}.txt
    end=`date +%s.%N`
-   diff ${f}.txt $dir/output
+   diff ${f}.txt $dir/output.longer
    elapsed="$(bc <<<"$end-$start")"
-   echo $elapsed
+   echo 'llvm w/ opt: '$elapsed
 
-   #java MiniCompiler $dir/${f}.mini -llvm
-   #clang $dir/${f}.ll -m32
-   #start=`date +%s.%N` 
-   #./a.out < $dir/input > ${f}.txt
-   #end=`date +%s.%N`
-   #elapsed="$(bc <<<"$end-$start")"
-   #echo $elapsed
-
-   #java MiniCompiler $dir/${f}.mini -llvm -sscp -uce
-   #clang $dir/${f}.ll -m32
+   #clang -S -O0 -Wno-everything $dir/${f}.c
    #start=`date +%s.%N`
-   #./a.out < $dir/input > ${f}.txt
+   #./a.out < $dir/input.longer > ${f}.txt
    #end=`date +%s.%N`
    #elapsed="$(bc <<<"$end-$start")"
-   #echo $elapsed
+   #echo 'clang w/o opt: '$elapsed
 
-   clang -S -O0 -Wno-everything $dir/${f}.c
-   start=`date +%s.%N`
-   ./a.out < $dir/input > ${f}.txt
-   end=`date +%s.%N`
-   elapsed="$(bc <<<"$end-$start")"
-   echo $elapsed
-
-   clang -S -O3 -Wno-everything $dir/${f}.c
-   start=`date +%s.%N`
-   ./a.out < $dir/input > ${f}.txt
-   end=`date +%s.%N`
-   elapsed="$(bc <<<"$end-$start")"
-   echo $elapsed
+   #clang -S -O3 -Wno-everything $dir/${f}.c
+   #start=`date +%s.%N`
+   #./a.out < $dir/input.longer > ${f}.txt
+   #end=`date +%s.%N`
+   #elapsed="$(bc <<<"$end-$start")"
+   #echo 'clang w/ opt: '$elapsed
 done
 
 
